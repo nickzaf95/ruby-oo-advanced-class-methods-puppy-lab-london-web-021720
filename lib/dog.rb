bf81f682031b1b2fb77c25deb672be7c87707f31
Nicholas Zafiriou <nicholaszafiriou@gmail.com>
2020-02-21 15:47:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+require 'pry'
+
+class Dog
+    @@all = []
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        i = 0
+        while i < @@all.size do 
+            puts @@all[i].name
+            i += 1
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.clear_all
+        @@all.clear 
+    end
+end
+
